d7ae4d8a52bcc8a8c1fb59fd8d2a78cce40b5c22
edder773 <st02045@naver.com>
2023-05-22 10:29:42 +0900

    DB수정

--- Back/frs_back/frs_back/settings.py
+++ Back/frs_back/frs_back/settings.py
@@ -78,6 +78,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
+        # 'articles.permissions.IsOwnerOrReadOnly',
     ],
 
 }
